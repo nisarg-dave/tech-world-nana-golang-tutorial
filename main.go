94b7f470601633463749e7219944d871fb81b039
Nisarg Dave <ndave630@gmail.com>
2025-01-25 17:19:21 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+// Main package is special because it indicates that the files belongs to an executable program
+package main
+
+// Import the fmt package, which contains functions for formatting and printing text. fmt is part of the standard library.
+import "fmt"
+
+// main is the entry point for the program
+// to run the program, use the command "go run main.go"
+func main() {
+	// Print is a function that prints a string to the console
+	fmt.Println("Hello, World!")
+}
